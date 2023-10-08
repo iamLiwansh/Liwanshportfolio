caa784e95b5cd5fa9989252d994cadba4d6a26ec
Liwansh <saini.liwansh@gmail.com>
2023-10-08 11:44:58 +0530

    my first commit reactwebsite

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,25 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/Liwansh Saini formal-modified.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          I bring proficiency in a variety of programming languages, including C++, Java, Python, and Node.js, enabling me to tackle diverse projects. I also have experience with web development, utilizing React and Next.js to create interactive, user-friendly applications.
+          </p>
+          <p className="hero--section-description">
+          My expertise extends to databases, with a strong grasp of MongoDB and MySQL for efficient data management. Currently immersed in my MCA program, I'm expanding my knowledge and honing my skills, blending theoretical insights with practical experience
+          </p>
+          <p className="hero--section-description">
+          I'm passionate about leveraging technology for innovation and problem-solving, and I'm eager to collaborate with like-minded professionals and mentors. Whether it's discussing tech trends, exploring opportunities, or sharing insights, I'm just a message away.
+          </p>
+          
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Liwansh</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          I'm currently pursuing a Master of Computer Applications (MCA) degree, <br /> and my journey in the realm of technology has been exhilarating.
+            
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/proimg-modified.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
